63042df4b2c1fbdb1e2b272b201a8c0c7c94da01
Rafael A. Rabelo <rafael.rabelo@thoughtworks.com>
2021-04-07 18:29:05 -0300

    refatora componente de alterna perfil

--- src/components/switchLayout.jsx
+++ src/components/switchLayout.jsx
@@ -21,57 +21,31 @@ class SwitchLayout extends React.Component {
     return (userRights === "root" || userRights === "admin") === true || false;
   }
 
+  switchButton(){
+    const {admLayout} = this.state;
+    return(
+      <div className="row">
+          <div className="col">
+          <span className="badge badge-pill badge-warning">{!admLayout ? "Adm" : "Usr"}</span>
+            <a
+              className={`btn btn-circle btn-sm btn-${!admLayout ? "" : "outline-"}secondary`}
+              href={admLayout ? "/user" : "/admin"}
+              id="layoutDropdown"
+            >
+              <i className="fas fa-desktop"></i>
+            </a>
+          </div>
+        </div>
+    );
+  }
+
   render() {
     const isAdm = this.shwowSwitchLayout();
+    const switchButton = this.switchButton();
+    
     return (
       <>
-        <ul
-          className="navbar-nav mr-auto"
-          style={{ display: isAdm ? "" : "none" }}
-        >
-          <li className="nav-item dropdown">
-            <div className="row">
-              <div className="col">
-                <button
-                  className={`btn btn-circle btn-sm btn-${
-                    !this.state.admLayout ? "" : "outline-"
-                  }secondary`}
-                  href="/#"
-                  id="layoutDropdown"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  <i className="fas fa-desktop"></i>
-                </button>
-                <div
-                  className="dropdown-menu dropdown-menu-right"
-                  aria-labelledby="layoutDropdown"
-                >
-                  <div className="col col-md-12 user-description">
-                    <div className="row">
-                      <div className="col col-md-12">Alterna visão para:</div>
-                    </div>
-                  </div>
-                  <div className="dropdown-divider"></div>
-                  <a
-                    className="dropdown-item"
-                    href={this.state.admLayout ? "/user" : "/admin"}
-                  >
-                    <div className="row">
-                      <div className="col col-md-2">
-                        <i className="fa fa-user-cog"></i>
-                      </div>
-                      <div className="col col-md-2">
-                        {!this.state.admLayout ? "Administrador" : "Usuario"}
-                      </div>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </li>
-        </ul>
+      {isAdm && switchButton}
       </>
     );
   }
