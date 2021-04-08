2b58882d8dd9af92084a464883be63554d5137c6
Rafael A. Rabelo <rafael.rabelo@thoughtworks.com>
2021-04-08 00:42:27 -0300

    refatora o header

--- src/pages/shared/components/pageHeader.jsx
+++ src/pages/shared/components/pageHeader.jsx
@@ -4,29 +4,17 @@ import "./css/admin-pet-classes.css";
 
 class PageHeader extends React.Component {
   render() {
-    const autoCloseClasses = true;
     return(
       <div className="col col-md-12">
           <div className="jumbotron">
-            <h1 className="display-6">Classes/ Familias</h1>
+            <h1 className="display-6">Gerencie</h1>
             <p className="lead">
-              Gerencie os cadastros de famílias/classes de pets.
+              Gerencie os cadastros.
             </p>
-            <br />
-            <hr className="my-4" />
             <p className="lead">
-              <button
-                className={`btn btn-sm btn-outline-${
-                  autoCloseClasses ? "success" : "secondary"
-                }`}
-                onClick={() => this.toggleAutoClose()}
-              >
-                <i className="fa fa-check"></i> Auto fechar guias
-              </button>{" "}
             </p>
             <small>
-              As configurações acima estão sendo utilizadas na paginas, ao
-              aterar talvez seja preciso recarregar a pagina para ter efeito.
+              Gerenciamento do administrador
             </small>
           </div>
         </div>
